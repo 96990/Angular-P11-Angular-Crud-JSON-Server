620abc9e4776b4eaf57abb01f1c39098a5dccf26
AbhijithKJ <kjabhijith.git@gmail.com>
2025-02-28 02:07:09 +0530

    main-commit-from-sarath-machine-2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,13 +2,14 @@ import { Component, inject } from '@angular/core';
 import { MessageModule } from 'primeng/message';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
 import { FormsModule, NgForm } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
-  imports: [MessageModule, InputTextModule, ButtonModule, FormsModule, NgIf],
+  imports: [MessageModule, InputTextModule, ButtonModule, PasswordModule, FormsModule, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -16,6 +17,8 @@ export class LoginComponent {
 
   private loginService = inject(LoginService);
   onSubmit(form: NgForm) {
+    const username = form.value.username;
+    const password = form.value.password;
     console.log(form.value);
   }
 }
