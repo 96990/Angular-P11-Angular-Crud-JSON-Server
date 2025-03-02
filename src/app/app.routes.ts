17865c52726c1fc1dae9ac8f94664fcfd2476516
AbhijithKJ <kjabhijith.git@gmail.com>
2025-03-03 00:43:18 +0530

    main-2 login, signup and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {path: 'login', loadComponent: () => import('./login/login.component').then(mod => mod.LoginComponent)},
+    {path: 'login', loadComponent: () => import('./auth/login/login.component').then(mod => mod.LoginComponent)},
+    {path: 'signup', loadComponent: () => import('./auth/signup/signup.component').then(mod => mod.SignupComponent)},
     {path: 'products', loadChildren: () => import('./product/product.routes').then(mod => mod.productRoutes)}
 ];
